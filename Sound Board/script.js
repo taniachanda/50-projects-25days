02b1d2bf024d0a089a89314d5b6a704094503f6f
Tania Chanda <taniachanda7@gmail.com>
2021-08-13 20:41:23 +0600

    two projects added

---
+++ Sound Board/script.js
@@ -0,0 +1,34 @@
+const sounds = [
+  'Anemo',
+  'Apocalypse',
+  'Battle',
+  'Berserker',
+  'Blood',
+  'Hall',
+  'Kings',
+  'Pure',
+  'Riffs',
+  'Through',
+]
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+  btn.addEventListener('click', () => {
+    stopSongs()
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0
+  })
+}
